a390ba006c1c33161d72202bb2bdbaad240a8e59
roryedwardson <roryedwardson@gmail.com>
2023-06-27 10:42:44 +0300

    updated file path in ingredient_generator.py to fix error

--- Resources/CustomLibs/ingredient_generator.py
+++ Resources/CustomLibs/ingredient_generator.py
@@ -2,7 +2,7 @@ import random
 
 
 def generate_ingredient():
-    words = open(r"C:\development\robot-scripts\recipeFinderV3\Resources\CustomLibs\ingredients.txt", "r")
+    words = open(r"C:\development\robot-scripts\recipeFinderV4\Resources\CustomLibs\ingredients.txt", "r")
     word_list = []
     for word in words:
         word = word.replace("\n", "")  # remove line breaks
